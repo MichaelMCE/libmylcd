f7ee31929d3503dc470f6c7e3bcb0a3c93b41133
MichaelMCE <mapei_@hotmail.com>
2019-11-24 00:43:59 +0000

    add common MIN()/MAX() to utils.h

--- examples/utils/utils.h
+++ examples/utils/utils.h
@@ -7,6 +7,11 @@
 #include <mylcd.h>
 
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+
+
 int utilInit ();
 void utilCleanup ();
 int utilInitConfig (char *configfile);
